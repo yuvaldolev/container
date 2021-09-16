a87cd12c02436cb26cf0f12e9d0a21148be43c39
Yuval Dolev <yuvaldolev2@gmail.com>
2021-09-17 01:29:47 +0300

    Hostname picking and socketpair creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use clap::Clap;
+
+use container::Opts;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opts = Opts::parse();
+    container::run(opts)?;
+
+    Ok(())
 }
